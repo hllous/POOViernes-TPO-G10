ef5f6e2cfa30e383daeb43f8824f29a2cae57849
nicof <nico.llousas@gmail.com>
2025-06-13 08:37:08 -0300

    Creacion de menu general

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
+import models.MainMenu;
 import models.MenuInicial;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Snake Game");
+        /*JFrame frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setContentPane(new MenuInicial(frame)); // ← Asegurate de pasar el frame
         frame.pack();
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        frame.setVisible(true);*/
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception ignored) {}
+
+        // Launch MainMenu frame
+        SwingUtilities.invokeLater(() -> {
+            MainMenu menu = new MainMenu();
+            menu.setVisible(true);
+        });
     }
 }
 
